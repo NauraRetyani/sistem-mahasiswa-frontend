70667a2582a86843ed569b02d4dc97ed28d024b1
apip11 <fatchullahwahid@gmail.com>
2022-09-29 08:05:56 +0700

    ganti judul jurusan

--- src/pages/Jurusan/JurusanList.jsx
+++ src/pages/Jurusan/JurusanList.jsx
@@ -6,7 +6,7 @@ export default function JurusanList() {
         <div>
             <div className="card shadow mb-4">
                 <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 className="m-0 font-weight-bold text-primary">Mata Kuliah</h6>
+                    <h6 className="m-0 font-weight-bold text-primary">Jurusan</h6>
 
                     <Link to="/jurusan/form">
                         <button className="btn btn-secondary">
