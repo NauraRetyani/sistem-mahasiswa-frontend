b9a1614d46f87c6f55fd5cc265ca3aabcde2d171
apip11 <fatchullahwahid@gmail.com>
2022-09-30 11:42:28 +0700

    update matkul

--- src/pages/Matkul/MatkulForm.jsx
+++ src/pages/Matkul/MatkulForm.jsx
@@ -1,7 +1,87 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
 export default function MatkulForm() {
+    const navigate = useNavigate()
+    const params = useParams()
+
+    const isEditing = params.idMatkul
+
+    const [option, setOption] = useState([])
+    const [formInput, setFormInput] = useState({
+        namaMatkul: ''
+    })
+
+    function handleInput(evt, propName) {
+        const copyFormInput = { ...formInput }
+        copyFormInput[propName] = evt.target.value
+        setFormInput(copyFormInput)
+    }
+
+    async function getOption() {
+        const res = await axios.get('https://sistem-mahasiswa-new.herokuapp.com/matkul/listmatkul')
+        setOption(res.data)
+    }
+
+    async function getFormInput () {
+        axios.get('https://sistem-mahasiswa-new.herokuapp.com/matkul/' + params.idMatkul)
+        setFormInput(res.data)
+    }
+
+    async function submitData(evt) {
+        evt.preventDefault()
+
+        if (isEditing) {
+            await axios.post('https://sistem-mahasiswa-new.herokuapp.com/matkul/savematkul' + params.idMatkul, formInput)
+        } else {
+            await axios.post('https://sistem-mahasiswa-new.herokuapp.com/matkul/savematkul', formInput)
+        }
+        navigate('/matkul')
+    }
+
+    useEffect(() => {
+        getOption()
+        if (isEditing) {
+            getFormInput()
+        }
+    }, [])
+
     return <>
-    <div>
-        ini Matkul Form
-    </div>
+        <div className="card shadow mb-4">
+            <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 className="m-0 font-weight-bold text-primary">FORM MATA KULIAH</h6>
+                <Link to="/matkul">
+                    <button className="btn btn-secondary">
+                        Kembali
+                    </button>
+                </Link>
+
+            </div>
+            <div className="card-body">
+                <form className="w-25" onSubmit={submitData}>
+                    <div className="form-group">
+                        <label>Nama Mata Kuliah</label>
+                        <select
+                            className="form-control"
+                            required
+                            value={formInput.namaMatkul}
+                            onChange={evt => handleInput(evt, 'namaMatkul')} >
+
+                            <option value="" disabled ></option>
+                            {option.map(item =>
+                                <option value={item.idMatkul}>
+                                    item.namaMatkul
+                                </option>
+                            )}
+                        </select>
+                    </div>
+                    <button className="btn btn-primary">
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </div>
     </>
 }
--- src/pages/Matkul/MatkulList.jsx
+++ src/pages/Matkul/MatkulList.jsx
@@ -1,7 +1,83 @@
+import axios from "axios"
+import { useEffect } from "react";
+import { useState } from "react"
+import { Link } from "react-router-dom";
+import Spinner from "../../components/Spinner/Spinner";
+
 export default function MatkulList() {
+    const [dataList, setDataList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    async function getDataList() {
+        try {
+            setIsLoading(true)
+            const res = await axios.get('https://sistem-mahasiswa-new.herokuapp.com/matkul/listmatkul')
+            setDataList(res.data)
+        } catch (err) {
+            alert('Terjadi Kesalahan')
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+
+    async function deleteData(idMatkul) {
+        await axios.delete('https://sistem-mahasiswa-new.herokuapp.com/matkul/' + idMatkul)
+        getDataList()
+    }
+
+    useEffect(() => {
+        getDataList()
+    }, [])
+
     return <>
-    <div>
-        ini Matkul List
-    </div>
+        <div className="card shadow mb-4">
+            <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 className="m-0 font-weight-bold text-primary">LIST MATKUL</h6>
+                <Link to="/matkul/form">
+                    <button className="btn btn-primary">
+                        Tambah
+                    </button>
+                </Link>
+
+            </div>
+            <div className="card-body">
+
+                {isLoading
+                    ? <Spinner />
+                    :
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">Nama Matkul</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {dataList.map((item, index) =>
+                                <tr>
+                                    <th scope="row">{index + 1}</th>
+                                    <td>{item.namaMatkul}</td>
+                                    <td>
+                                        <Link to={'/matkul/form/' + item.idMatkul}>
+                                            <button className="button" class="btn btn-success">
+                                                Edit
+                                            </button>
+                                        </Link>
+                                        &nbsp; &nbsp;
+
+                                        <button className="button" class="btn btn-danger"
+                                            onClick={() => deleteData(item.idMatkul)}>
+                                            Hapus
+                                        </button>
+                                    </td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                }
+            </div>
+        </div>
     </>
 }
